74c535b1059598973fd3c4a2272bdaf6b922c211
DennisPaul01 <denys_paul08@yahoo.com>
2022-01-02 20:23:04 +0200

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import ParaStart from "./Components/ParaStart";
 
 function App() {
   return (
     <div className="App">
       <p>Denis App Gallery</p>
+      <ParaStart></ParaStart>
     </div>
   );
 }
---
+++ src/Components/ParaStart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./ParaStart.module.scss";
+
+const ParaStart = () => {
+  return <p className={classes.paragraph}>Denis Paul WEB Desing</p>;
+};
+
+export default ParaStart;
