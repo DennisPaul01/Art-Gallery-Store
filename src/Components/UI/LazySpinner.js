569690b0038ce57e8071510a853899ca50ac02e4
DennisPaul01 <denys_paul08@yahoo.com>
2022-01-11 21:45:59 +0200

    Final Product2

--- src/Components/UI/LazySpinner.js
+++ src/Components/UI/LazySpinner.js
@@ -4,12 +4,14 @@ import classes from "./LazySpinner.module.scss";
 
 const LazySpinner = () => {
   return (
-    <div className={classes.ldsRing}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <section className={classes.backgroundSpinner}>
+      <div className={classes.ldsRing}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
   );
 };
 
